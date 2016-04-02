7b5162bde1df09addf135e93b9abe56ef5c6b5eb
linzhiheng <linx-x@live.com>
2016-04-02 22:32:32 +0800

    更新海浪运动计算程序

--- Swell_Wave_Motion.m
+++ Swell_Wave_Motion.m
@@ -1,8 +1,11 @@
 %Swell Wave Motion
 
 amplitude = 1;
-angularFrewuency = 1;
+angularFrewuency = 2;
 waveNumber = 1;
 vctorTerm = 1+1i;
 
-waveFunction = @(x,z,t)-1*amplitude*vctorTerm*exp();
+waveFunction = @(x,z,t)-1*amplitude*vctorTerm*exp(i*angularFrewuency*t...
+                - i*waveNumber*x - waveNumber*z);
+            
+waveFunction(1,1,1)
