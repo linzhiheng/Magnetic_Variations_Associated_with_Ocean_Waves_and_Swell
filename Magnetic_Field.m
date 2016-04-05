40c4ac5ac45e4ff5f8ab21fae8a88e6289c7875d
linzhiheng <linx-x@live.com>
2016-04-05 11:21:29 +0800

    更新

--- Magnetic_Field.m
+++ Magnetic_Field.m
@@ -16,11 +16,18 @@ Gamma = 4*pi*conductivity*angularFrewuency;
 Beta = Gamma/(waveNumber*waveNumber);
 B = sqrt(1+(i*Beta));
 
+% z > 0
 %Vertical Component of Magnetic Field
-hZFunction = @(z) i*A*( (2/(1+B)) * exp(-waveNumber*z*B)...
+hZDownFunction = @(z) i*A*( (2/(1+B)) * exp(-waveNumber*z*B)...
                           - exp(-waveNumber*z) );
 
 %Horizontal Component of Magnetic Field
-hXFunction = @(z) -1*A*( ((2*B)/(1+B)) * exp(-waveNumber*z*B)...
+hXDownFunction = @(z) -1*A*( ((2*B)/(1+B)) * exp(-waveNumber*z*B)...
                           - exp(-waveNumber*z) );
 
+% z < 0
+%Vertical Component of Magnetic Field
+hZUpFunction = @(z) (-1*A/Beta) * ((1-B)^2) * exp(waveNumber*z);
+
+%Horizontal Component of Magnetic Field
+hXUpFunction = @(z) (i*A/Beta) * ((1-B)^2) * exp(waveNumber*z);
