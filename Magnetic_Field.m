e8d3eb582f93a383d902e2b53590e06dc6037746
linzhiheng <linx-x@live.com>
2016-04-05 11:05:18 +0800

    创建磁场计算程序

---
+++ Magnetic_Field.m
@@ -0,0 +1,23 @@
+earthField = 1;
+dip_EarthField = 5;
+inclination_EarthField = 20;
+
+amplitude = 1;
+angularFrewuency = 2;
+waveNumber = 1;
+
+conductivity = 1;
+
+S = sind(dip_EarthField);
+C = cosd(dip_EarthField)*cosd(inclination_EarthField);
+A = amplitude*waveNumber*earthField*(S+(i*C));
+
+Gamma = 4*pi*conductivity*angularFrewuency;
+Beta = Gamma/(waveNumber*waveNumber);
+B = sqrt(1+(i*Beta));
+
+%Vertical Component of Magnetic Field
+
+hzFunction = @(z) i*A*( (2/(1+B)) * exp(-waveNumber*z*B)...
+                          - exp(-waveNumber*z) );
+
--- Swell_Wave_Motion.m
+++ Swell_Wave_Motion.m
@@ -5,7 +5,7 @@ angularFrewuency = 2;
 waveNumber = 1;
 vctorTerm = 1+1i;
 
-waveFunction = @(x,z,t)-1*amplitude*vctorTerm*exp(i*angularFrewuency*t...
+waveFunction = @(x,z,t) -1*amplitude*vctorTerm*exp(i*angularFrewuency*t...
                 - i*waveNumber*x - waveNumber*z);
             
 waveFunction(1,1,1)
